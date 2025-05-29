901896648039e880bc103f1e2319ba2f5a5c3ca2
Aman <164004893+notbadIam@users.noreply.github.com>
2025-05-29 17:46:44 +0530

    Update modeldb.py

--- app/modeldb.py
+++ app/modeldb.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import text
-from config import DATABASE_URL
+from app.config import DATABASE_URL
 
 
 engine = create_engine(DATABASE_URL)
