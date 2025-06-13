884bb13882eab2f5f3b4e4e110a8b79985d16310
Aman <164004893+notbadIam@users.noreply.github.com>
2025-06-13 11:52:50 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,8 +13,10 @@ app = FastAPI()
 
 from fastapi.middleware.cors import CORSMiddleware
 
-"http://127.0.0.1:5500",
-    "http://localhost:5500"
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
 
 app.add_middleware(
     CORSMiddleware,
