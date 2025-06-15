9819cb5a83a42c8446203a83c8d9e64e5da21290
Aman <164004893+notbadIam@users.noreply.github.com>
2025-06-15 10:00:29 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://book-nest-h7nw.onrender.com",
 ]
 
 app.add_middleware(
